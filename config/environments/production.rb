063f9e0196b163949a2f214e96e04488db3ef0d2
Aaron Lipman <alipman88@gmail.com>
2015-02-09 11:01:04 -0500

    Use AWS S3 for Paperclip storage in Production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   Paperclip::Attachment.default_options.merge!({
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET'],
+    :s3_credentials => {
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    }
+  })
 end
