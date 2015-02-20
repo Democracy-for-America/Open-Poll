46875676f2cc9adf3c9d1cefa2f7c508e7358c74
Aaron Lipman <alipman88@gmail.com>
2015-02-20 10:55:42 -0500

    enable cloudflare middleware to correctly log IP addresses.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module OpenPoll
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    require "#{Rails.root}/lib/cloud_flare_middleware"
+    config.middleware.insert_before(0, Rack::CloudFlareMiddleware) 
   end
 end
