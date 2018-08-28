ed36b324c0ea1ed47e3baeceef46e1085ef05732
Aaron Lipman <alipman88@gmail.com>
2018-08-28 16:23:37 -0400

    Add candidate descriptions to non-visual page.
    Also, various CSS tweaks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def embed_videos text
-    text.to_s.gsub(/https?\:\/\/(www\.)?youtube.com\/watch\?v=([a-zA-Z0-9]+)/, '<div style="text-align: center;"><iframe width="560" height="315" style="max-width: 100%;" src="https://www.youtube.com/embed/\2" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>').html_safe
+    text.to_s.gsub(/https?\:\/\/(www\.)?youtube.com\/watch\?v=([a-zA-Z0-9]+)/, '<div class="videoWrapper"><iframe width="560" height="315" style="max-width: 100%;" src="https://www.youtube.com/embed/\2" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>').html_safe
   end
 end
