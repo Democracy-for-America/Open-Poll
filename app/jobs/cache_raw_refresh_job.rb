3023295395fea38dcdb846711a4f355c124dad63
Aaron Lipman <alipman88@gmail.com>
2018-09-25 22:13:08 -0400

    Add extra stats to raw results dash.

--- app/jobs/cache_raw_refresh_job.rb
+++ app/jobs/cache_raw_refresh_job.rb
@@ -2,7 +2,8 @@ class CacheRawRefreshJob < ActiveJob::Base
   queue_as :default
 
   def perform(poll)
-    Rails.cache.write("raw/total/#{poll.id}", poll.votes.count)
+    Rails.cache.write("raw/total_voters/#{poll.id}", poll.total_voters)
+    Rails.cache.write("raw/total_votes/#{poll.id}", poll.total_votes)
     Rails.cache.write("raw/results/#{poll.id}", poll.raw_results)
     Rails.cache.write("raw/timestamp/#{poll.id}", Time.now)
   end
